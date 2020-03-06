ca533675e894770245720fafc8075bc3bf33d260
dyceit <814577465@qq.com>
2020-03-06 13:06:56 +0800

    init

---
+++ code2snippet/code2snippet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+  <!-- The template root requires exactly one element -->
+  </div>
+</template>
+
+<script>
+export default {
+name: '$TM_FILENAME_BASE'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
